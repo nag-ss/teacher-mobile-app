fbb8a27fa45d0eef25dd3c18bce65c860d1f9a7f
K Ranganath <ranganathkomandur@gmail.com>
2025-04-24 18:04:22 +0530

    Add the AI Check Modal

--- app/settings/index.tsx
+++ app/settings/index.tsx
@@ -3,12 +3,14 @@ import { Text, Button } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import SummaryModal from '@/components/Modals/Modal_1_SummaryModal';
 import ClassTaskCardPop from '@/components/Modals/Modal_2_ClassTaskModal';
-import NewTaskModal from '@/components/Modals/Modal_3_CreateTaskModal' 
+import NewTaskModal from '@/components/Modals/Modal_3_CreateTaskModal';
+import AiCheckModal from '@/components/Modals/Modal_4_AICheckModal';
 
 const Settings = () => {
   const [showModal1SummaryModal, setShowModal1SummaryModal] = useState(false);
   const [showModal2TasksModal, setShowModal2TasksModal] = useState(false);
   const [showModal3NewTasksModal, setShowModal3NewTaskModal] = useState(false);
+  const [showModal4AICheckModal, setShowModal4AICheckModal] = useState(false);
 
   const showDetailsModal = () => {
     setShowModal1SummaryModal(false)
@@ -32,9 +34,8 @@ const Settings = () => {
 
   const gotoTask = (taskTitle: string) => {
     if(taskTitle == 'AI') {
-        // setCreateTaskModalOpen(false)
-        // setShowAITaskModal(true)
-        alert("AI Clicked")
+        setShowModal3NewTaskModal(false)
+        setShowModal4AICheckModal(true)
     } else if(taskTitle == 'Slip Test') {
         // setCreateTaskModalOpen(false)
         // setCreateSlipTestModal(true)
@@ -44,7 +45,13 @@ const Settings = () => {
         // setShowCWTaskModal(true)
         alert("Classwork Clicked")
     }
-}
+  }
+
+  const backToNewTasksModal = () => {
+    setShowModal4AICheckModal(false)
+    setShowModal3NewTaskModal(true)
+  }
+
   
 
   return (
@@ -54,6 +61,7 @@ const Settings = () => {
         <SummaryModal visible={showModal1SummaryModal} onClose={() => setShowModal1SummaryModal(false)} clickedNext={showDetailsModal} />
         <ClassTaskCardPop visible={showModal2TasksModal} onClose={() => setShowModal2TasksModal(false)} goBack={backToSummaryModal} addTask={showAddTaskModal} />
         <NewTaskModal visible={showModal3NewTasksModal} onClose={() => setShowModal3NewTaskModal(false)} goBack={backToShowDetailsModal} clickedNext={gotoTask} />
+        <AiCheckModal visible={showModal4AICheckModal} onClose={() => setShowModal4AICheckModal(false)} goBack={backToNewTasksModal} />
       </SafeAreaView>
     </SafeAreaProvider>
   );
